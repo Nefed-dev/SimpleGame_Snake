cf2774dbe89352fa63854fac7464bc84a4553d1b
nefedov-it <69164416+nefedov-it@users.noreply.github.com>
2021-05-27 18:01:05 +0300

    Add skeleton of game

---
+++ snake.py
@@ -0,0 +1,23 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class Cube:
+    pass
+class Snake:
+    pass
+
+def draw_grid():
+    pass
+def draw_window():
+    pass
+def random_snake():
+    pass
+def random_snake():
+    pass
+def message_box():
+    pass
+def main():
+    pass
